984fdb52882f6c43a584e630485e67b6ea2716d7
user11681 <username11681@gmail.com>
2020-09-21 23:23:23 +0100

    add defineClass

--- src/main/java/user11681/reflect/Reflect.java
+++ src/main/java/user11681/reflect/Reflect.java
@@ -3,24 +3,68 @@ package user11681.reflect;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodType;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.nio.ByteBuffer;
+import java.security.ProtectionDomain;
 import java.util.HashMap;
 import net.gudenau.lib.unsafe.Unsafe;
 
 public class Reflect {
     public static final boolean JAVA_9;
 
+    private static final MethodHandle defineClass0;
+    private static final MethodHandle defineClass1;
+    private static final MethodHandle defineClass2;
+    private static final MethodHandle defineClass3;
     private static final MethodHandle getDeclaredFields0;
 
+    private static final boolean DEFINE_CLASS_HAS_BOOLEAN;
     private static final int MODIFIERS_OFFSET;
 
-    public static void main(final String[] args) {}
+    public static <T> Class<T> defineClass(final ClassLoader classLoader, final byte[] bytecode) {
+        try {
+            return (Class<T>) defineClass0.invokeExact(classLoader, bytecode, 0, bytecode.length);
+        } catch (final Throwable throwable) {
+            throw new RuntimeException(throwable);
+        }
+    }
+
+    public static <T> Class<T> defineClass(final ClassLoader classLoader, final byte[] bytecode, final int offset, final int length) {
+        try {
+            return (Class<T>) defineClass0.invokeExact(classLoader, bytecode, offset, length);
+        } catch (final Throwable throwable) {
+            throw new RuntimeException(throwable);
+        }
+    }
+
+    public static <T> Class<T> defineClass(final ClassLoader classLoader, final String name, final byte[] bytecode, final int offset, final int length) {
+        try {
+            return (Class<T>) defineClass1.invokeExact(classLoader, name, bytecode, offset, length);
+        } catch (final Throwable throwable) {
+            throw new RuntimeException(throwable);
+        }
+    }
+
+    public static <T> Class<T> defineClass(final ClassLoader classLoader, final String name, final byte[] bytecode, final int offset, final int length, final ProtectionDomain protectionDomain) {
+        try {
+            return (Class<T>) defineClass2.invokeExact(classLoader, name, bytecode, offset, length, protectionDomain);
+        } catch (final Throwable throwable) {
+            throw new RuntimeException(throwable);
+        }
+    }
+
+    public static <T> Class<T> defineClass(final ClassLoader classLoader, final String name, final ByteBuffer bytecode, final ProtectionDomain protectionDomain) {
+        try {
+            return (Class<T>) defineClass3.invokeExact(classLoader, name, bytecode, protectionDomain);
+        } catch (final Throwable throwable) {
+            throw new RuntimeException(throwable);
+        }
+    }
 
     public static Field getAccessibleField(final Class<?> klass, final String name) {
         try {
-            final Field[] fields = (Field[]) getDeclaredFields0.invokeExact(klass, false);
-
-            for (final Field field : fields) {
+            for (final Field field : getFields(klass)) {
                 if (name.equals(field.getName())) {
                     field.setAccessible(true);
 
@@ -38,9 +82,7 @@ public class Reflect {
 
     public static Field getField(final Class<?> klass, final String name) {
         try {
-            final Field[] fields = (Field[]) getDeclaredFields0.invokeExact(klass, false);
-
-            for (final Field field : fields) {
+            for (final Field field : getFields(klass)) {
                 if (name.equals(field.getName())) {
                     return field;
                 }
@@ -54,7 +96,7 @@ public class Reflect {
 
     public static Field[] getAccessibleFields(final Class<?> klass) {
         try {
-            final Field[] fields = (Field[]) getDeclaredFields0.invokeExact(klass, false);
+            final Field[] fields = getFields(klass);
 
             for (final Field field : fields) {
                 field.setAccessible(true);
@@ -70,15 +112,9 @@ public class Reflect {
 
     public static Field[] getFields(final Class<?> klass) {
         try {
-            return (Field[]) getDeclaredFields0.invokeExact(klass, false);
-        } catch (final Throwable throwable) {
-            throw new RuntimeException(throwable);
-        }
-    }
-
-    public static Field[] getFields(final Class<?> klass, final boolean publicOnly) {
-        try {
-            return (Field[]) getDeclaredFields0.invokeExact(klass, publicOnly);
+            return DEFINE_CLASS_HAS_BOOLEAN
+                ? (Field[]) getDeclaredFields0.invokeExact(klass, false)
+                : (Field[]) getDeclaredFields0.invokeExact(klass);
         } catch (final Throwable throwable) {
             throw new RuntimeException(throwable);
         }
@@ -95,8 +131,29 @@ public class Reflect {
                 Unsafe.putObjectVolatile(IllegalAccessLogger, Unsafe.staticFieldOffset(logger), null);
             }
 
-            getDeclaredFields0 = Unsafe.trustedLookup.findVirtual(Class.class, "getDeclaredFields0", MethodType.methodType(Field[].class, boolean.class));
+            defineClass0 = Unsafe.trustedLookup.findVirtual(ClassLoader.class, "defineClass", MethodType.methodType(Class.class, byte[].class, int.class, int.class));
+            defineClass1 = Unsafe.trustedLookup.findVirtual(ClassLoader.class, "defineClass", MethodType.methodType(Class.class, String.class, byte[].class, int.class, int.class));
+            defineClass2 = Unsafe.trustedLookup.findVirtual(ClassLoader.class, "defineClass", MethodType.methodType(Class.class, String.class, byte[].class, int.class, int.class, ProtectionDomain.class));
+            defineClass3 = Unsafe.trustedLookup.findVirtual(ClassLoader.class, "defineClass", MethodType.methodType(Class.class, String.class, ByteBuffer.class, ProtectionDomain.class));
 
+            final Method[] methods = Class.class.getDeclaredMethods();
+            Method found = null;
+
+            for (final Method method : methods) {
+                if ((method.getModifiers() & Modifier.NATIVE) != 0 && method.getReturnType() == Field[].class) {
+                    found = method;
+
+                    break;
+                }
+            }
+
+            if (found == null) {
+                throw new Throwable();
+            } else {
+                getDeclaredFields0 = Unsafe.trustedLookup.unreflectSpecial(found, Class.class);
+            }
+
+            DEFINE_CLASS_HAS_BOOLEAN = found.getParameterCount() > 0;
             MODIFIERS_OFFSET = (int) Unsafe.objectFieldOffset(getField(Field.class, "modifiers"));
 
             final Class<?> Reflection = Class.forName(JAVA_9 ? "jdk.internal.reflect.Reflection" : "sun.reflect.Reflection");
--- src/test/java/user11681/reflect/ReflectTest.java
+++ src/test/java/user11681/reflect/ReflectTest.java
@@ -1,11 +1,20 @@
 package user11681.reflect;
 
+import java.io.InputStream;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.commons.annotation.Testable;
 
 @Testable
 class ReflectTest {
     @Test
-    void test() {
+    void test() throws Throwable {
+        final InputStream stream = ReflectTest.class.getResourceAsStream("/user11681/reflect/ReflectTest$T.class");
+        final byte[] bytecode = new byte[stream.available()];
+
+        while (stream.read(bytecode) != -1) {}
+
+        Reflect.defineClass(ReflectTest.class.getClassLoader(), bytecode);
     }
+
+    static class T {}
 }
