1ac74d14f68d4fa7d71ae5524b2e7ccd886121e4
user11681 <username11681@gmail.com>
2020-09-24 06:38:08 +0100

    field access utilties

--- src/main/java/user11681/reflect/Reflect.java
+++ src/main/java/user11681/reflect/Reflect.java
@@ -22,17 +22,161 @@ public class Reflect {
     private static final boolean DEFINE_CLASS_HAS_BOOLEAN;
     private static final int MODIFIERS_OFFSET;
 
-    public static <T> Class<T> defineClass(final ClassLoader classLoader, final byte[] bytecode) {
-        try {
-            return (Class<T>) defineClass0.invokeExact(classLoader, bytecode, 0, bytecode.length);
-        } catch (final Throwable throwable) {
-            throw new RuntimeException(throwable);
-        }
+    public static int getInt(final Field field) {
+        return Unsafe.getInt(field.getDeclaringClass(), Unsafe.staticFieldOffset(field));
+    }
+
+    public static void putInt(final Field field, final int value) {
+        Unsafe.putInt(field.getDeclaringClass(), Unsafe.staticFieldOffset(field), value);
+    }
+
+    public static int getInt(final Object object, final Field field) {
+        return Unsafe.getInt(object, Unsafe.objectFieldOffset(field));
+    }
+
+    public static void putInt(final Object object, final Field field, final int value) {
+        Unsafe.putInt(object, Unsafe.objectFieldOffset(field), value);
+    }
+
+    public static Object getObject(final Field field) {
+        return Unsafe.getObject(field.getDeclaringClass(), Unsafe.staticFieldOffset(field));
+    }
+
+    public static void putObject(final Field field, final Object value) {
+        Unsafe.putObject(field.getDeclaringClass(), Unsafe.staticFieldOffset(field), value);
+    }
+
+    public static Object getObject(final Object object, final Field field) {
+        return Unsafe.getObject(object, Unsafe.objectFieldOffset(field));
+    }
+
+    public static void putObject(final Object object, final Field field, final Object value) {
+        Unsafe.putObject(object, Unsafe.objectFieldOffset(field), value);
+    }
+
+    public static boolean getBoolean(final Field field) {
+        return Unsafe.getBoolean(field.getDeclaringClass(), Unsafe.staticFieldOffset(field));
+    }
+
+    public static void putBoolean(final Field field, final boolean value) {
+        Unsafe.putBoolean(field.getDeclaringClass(), Unsafe.staticFieldOffset(field), value);
+    }
+
+    public static boolean getBoolean(final Object object, final Field field) {
+        return Unsafe.getBoolean(object, Unsafe.objectFieldOffset(field));
+    }
+
+    public static void putBoolean(final Object object, final Field field, final boolean value) {
+        Unsafe.putBoolean(object, Unsafe.objectFieldOffset(field), value);
+    }
+
+    public static byte getByte(final Field field) {
+        return Unsafe.getByte(field.getDeclaringClass(), Unsafe.staticFieldOffset(field));
+    }
+
+    public static void putByte(final Field field, final byte value) {
+        Unsafe.putByte(field.getDeclaringClass(), Unsafe.staticFieldOffset(field), value);
+    }
+
+    public static byte getByte(final Object object, final Field field) {
+        return Unsafe.getByte(object, Unsafe.objectFieldOffset(field));
+    }
+
+    public static void putByte(final Object object, final Field field, final byte value) {
+        Unsafe.putByte(object, Unsafe.objectFieldOffset(field), value);
+    }
+
+    public static short getShort(final Field field) {
+        return Unsafe.getShort(field.getDeclaringClass(), Unsafe.staticFieldOffset(field));
+    }
+
+    public static void putShort(final Field field, final short value) {
+        Unsafe.putShort(field.getDeclaringClass(), Unsafe.staticFieldOffset(field), value);
+    }
+
+    public static short getShort(final Object object, final Field field) {
+        return Unsafe.getShort(object, Unsafe.objectFieldOffset(field));
+    }
+
+    public static void putShort(final Object object, final Field field, final short value) {
+        Unsafe.putShort(object, Unsafe.objectFieldOffset(field), value);
+    }
+
+    public static char getChar(final Object object, final Field field) {
+        return Unsafe.getChar(object, Unsafe.objectFieldOffset(field));
+    }
+
+    public static void putChar(final Field field, final char value) {
+        Unsafe.putChar(field.getDeclaringClass(), Unsafe.staticFieldOffset(field), value);
+    }
+
+    public static char getChar(final Field field) {
+        return Unsafe.getChar(field.getDeclaringClass(), Unsafe.staticFieldOffset(field));
+    }
+
+    public static void putChar(final Object object, final Field field, final char value) {
+        Unsafe.putChar(object, Unsafe.objectFieldOffset(field), value);
+    }
+
+    public static long getLong(final Field field) {
+        return Unsafe.getLong(field.getDeclaringClass(), Unsafe.staticFieldOffset(field));
+    }
+
+    public static void putLong(final Field field, final long value) {
+        Unsafe.putLong(field.getDeclaringClass(), Unsafe.staticFieldOffset(field), value);
+    }
+
+    public static long getLong(final Object object, final Field field) {
+        return Unsafe.getLong(object, Unsafe.objectFieldOffset(field));
+    }
+
+    public static void putLong(final Object object, final Field field, final long value) {
+        Unsafe.putLong(object, Unsafe.objectFieldOffset(field), value);
+    }
+
+    public static float getFloat(final Field field) {
+        return Unsafe.getFloat(field.getDeclaringClass(), Unsafe.staticFieldOffset(field));
+    }
+
+    public static void putFloat(final Field field, final float value) {
+        Unsafe.putFloat(field.getDeclaringClass(), Unsafe.staticFieldOffset(field), value);
+    }
+
+    public static float getFloat(final Object object, final Field field) {
+        return Unsafe.getFloat(object, Unsafe.objectFieldOffset(field));
+    }
+
+    public static void putFloat(final Object object, final Field field, final float value) {
+        Unsafe.putFloat(object, Unsafe.objectFieldOffset(field), value);
+    }
+
+    public static double getDouble(final Field field) {
+        return Unsafe.getDouble(field.getDeclaringClass(), Unsafe.staticFieldOffset(field));
+    }
+
+    public static void putDouble(final Field field, final double value) {
+        Unsafe.putDouble(field.getDeclaringClass(), Unsafe.staticFieldOffset(field), value);
+    }
+
+    public static double getDouble(final Object object, final Field field) {
+        return Unsafe.getDouble(object, Unsafe.objectFieldOffset(field));
+    }
+
+    public static void putDouble(final Object object, final Field field, final double value) {
+        Unsafe.putDouble(object, Unsafe.objectFieldOffset(field), value);
     }
 
     public static <T> Class<T> defineClass(final ClassLoader classLoader, final byte[] bytecode, final int offset, final int length) {
         try {
             return (Class<T>) defineClass0.invokeExact(classLoader, bytecode, offset, length);
+        } catch (final Throwable throwable) {
+            throw new RuntimeException(throwable);
+        }
+    }
+
+    public static <T> Class<T> defineClass(final ClassLoader classLoader, final String name, final byte[] bytecode) {
+        try {
+            return (Class<T>) defineClass1.invokeExact(classLoader, name, bytecode, 0, bytecode.length);
         } catch (final Throwable throwable) {
             throw new RuntimeException(throwable);
         }
--- src/test/java/user11681/reflect/ReflectTest.java
+++ src/test/java/user11681/reflect/ReflectTest.java
@@ -1,11 +1,16 @@
 package user11681.reflect;
 
 import java.io.InputStream;
+import net.gudenau.lib.unsafe.Unsafe;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.commons.annotation.Testable;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
 
 @Testable
 class ReflectTest {
+    static final Logger logger = LoggerFactory.getLogger(ReflectTest.class);
+
     @Test
     void test() throws Throwable {
         final InputStream stream = ReflectTest.class.getResourceAsStream("/user11681/reflect/ReflectTest$T.class");
@@ -13,8 +18,17 @@ class ReflectTest {
 
         while (stream.read(bytecode) != -1) {}
 
-        Reflect.defineClass(ReflectTest.class.getClassLoader(), bytecode);
+        final Class<T> klass = Reflect.defineClass((ClassLoader) Reflect.getObject(ReflectTest.class, Reflect.getField(Class.class, "classLoader")), "user11681.reflect.ReflectTest$T", bytecode);
+        final Object t = Unsafe.allocateInstance(klass);
+
+        logger.info(() -> String.valueOf(Reflect.getInt(t, Reflect.getField(klass, "thing"))));
+
+        Reflect.putInt(t, Reflect.getField(klass, "thing"), 12222);
+
+        logger.info(() -> String.valueOf(Reflect.getInt(t, Reflect.getField(klass, "thing"))));
     }
 
-    static class T {}
+    static class T {
+        private final int thing = 1;
+    }
 }
