8d244c3f4d1eecc183cb243e64b4a4e43bf44077
Mohamed Maaliki <me@auoeke.net>
2022-04-04 23:56:57 +0100

    rename `EntryTest` to `Warmup` and fix

---
+++ test/test/Warmup.java
@@ -0,0 +1,23 @@
+package test;
+
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import net.auoeke.reflect.Reflect;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.annotation.Testable;
+
+@SuppressWarnings("NewClassNamingConvention")
+@Testable
+@Order(0)
+public class Warmup {
+    @Test
+    void load() {
+        var path = Path.of(Reflect.class.getProtectionDomain().getCodeSource().getLocation().toURI());
+
+        try (var list = Files.list((Files.isDirectory(path) ? path : FileSystems.newFileSystem(path).getPath("/")).resolve("net/auoeke/reflect"))) {
+            list.forEach(type -> Class.forName("net.auoeke.reflect." + type.getFileName().toString().replace(".class", "")));
+        }
+    }
+}
