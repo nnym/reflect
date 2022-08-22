07370e938c13ef8ad4da7695edc91d868c13a0c6
Mohamed Maaliki <me@auoeke.net>
2022-08-22 18:10:13 +0100

    stack trace modification utilities

--- source/net/auoeke/reflect/Exceptions.java
+++ source/net/auoeke/reflect/Exceptions.java
@@ -7,6 +7,8 @@ import java.util.function.Function;
  */
 class Exceptions {
     private static final Pointer detailMessage = Pointer.of(Throwable.class, "detailMessage");
+    private static final Pointer stackTrace = Pointer.of(Throwable.class, "stackTrace");
+    private static final StackTraceElement[] defaultStackTrace = stackTrace.getT(new Throwable());
 
     public static <T extends Throwable> T message(T trouble, String message) {
         detailMessage.putReference(trouble, message);
@@ -15,5 +17,18 @@ class Exceptions {
 
     public static <T extends Throwable> T message(T trouble, Function<String, String> transformer) {
         return message(trouble, transformer.apply(trouble.getMessage()));
+    }
+
+    public static <T extends Throwable> T stackTrace(T trouble, StackTraceElement[] trace) {
+        stackTrace.putReference(trouble, trace);
+        return trouble;
+    }
+
+    public static <T extends Throwable> T clearStackTrace(T trouble) {
+        return stackTrace(trouble, defaultStackTrace);
+    }
+
+    public static <T extends Throwable> T disableStackTrace(T trouble) {
+        return stackTrace(trouble, null);
     }
 }
