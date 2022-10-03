64bb588d05d97f73c284a4a5d74ea35f17e32627
Mohamed Maaliki <me@auoeke.net>
2022-10-03 04:00:11 +0100

    add mock `BigInterface`

---
+++ test/mock/BigInterface.java
@@ -0,0 +1,109 @@
+package mock;
+
+public interface BigInterface {
+	void a();
+
+	void b();
+
+	void c();
+
+	void d();
+
+	void e();
+
+	void f();
+
+	void g();
+
+	void h();
+
+	void i();
+
+	void j();
+
+	void k();
+
+	void l();
+
+	void m();
+
+	void n();
+
+	void o();
+
+	void p();
+
+	void q();
+
+	void r();
+
+	void s();
+
+	void t();
+
+	void u();
+
+	void v();
+
+	void w();
+
+	void x();
+
+	void y();
+
+	void z();
+
+	class Impl implements BigInterface {
+		@Override public void a() {}
+
+		@Override public void b() {}
+
+		@Override public void c() {}
+
+		@Override public void d() {}
+
+		@Override public void e() {}
+
+		@Override public void f() {}
+
+		@Override public void g() {}
+
+		@Override public void h() {}
+
+		@Override public void i() {}
+
+		@Override public void j() {}
+
+		@Override public void k() {}
+
+		@Override public void l() {}
+
+		@Override public void m() {}
+
+		@Override public void n() {}
+
+		@Override public void o() {}
+
+		@Override public void p() {}
+
+		@Override public void q() {}
+
+		@Override public void r() {}
+
+		@Override public void s() {}
+
+		@Override public void t() {}
+
+		@Override public void u() {}
+
+		@Override public void v() {}
+
+		@Override public void w() {}
+
+		@Override public void x() {}
+
+		@Override public void y() {}
+
+		@Override public void z() {}
+	}
+}
