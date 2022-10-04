764c06e6f3a88cf5e2c9be588518877c2e77f593
Mohamed Maaliki <me@auoeke.net>
2022-10-04 04:48:15 +0100

    add experimental `AccessibleObjects`

---
+++ test/experimental/AccessibleObjects.java
@@ -0,0 +1,13 @@
+package experimental;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.reflect.AccessibleObject;
+import net.auoeke.reflect.Invoker;
+
+public class AccessibleObjects {
+	private static final MethodHandle getRoot = Invoker.findVirtual(AccessibleObject.class, "getRoot", AccessibleObject.class);
+
+	public static <T extends AccessibleObject> T root(T object) {
+		return (T) getRoot.invokeExact(object);
+	}
+}
